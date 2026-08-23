0496da0201dd2d5dd8f13cc6dd28fa4329940fd9
Kiran-Kumari88 <146402849+Kiran-Kumari88@users.noreply.github.com>
2026-08-24 00:12:05 +0530

    Time: 27 ms (66.92%) | Memory: 36.5 MB (64.25%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(int ind, int target, vector<int>& coins,vector<vector<int>>& dp) {
+
+        if (ind == 0)
+            return (target % coins[0] == 0);
+
+        if (dp[ind][target] != -1) return dp[ind][target];
+
+        int notTake = func(ind - 1, target, coins, dp);
+        int take = 0;
+        if (coins[ind] <= target)
+            take = func(ind, target - coins[ind], coins, dp);
+
+        return dp[ind][target] = take + notTake;
+    }
+
+    int change(int amount, vector<int>& coins) {
+
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        return func(n - 1, amount, coins, dp);
+    }
+};
