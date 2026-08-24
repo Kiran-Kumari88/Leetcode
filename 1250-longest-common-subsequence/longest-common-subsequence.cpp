8515b6a2a3ce21a7ddd86a0e1957f7c5452e151c
Kiran-Kumari88 <146402849+Kiran-Kumari88@users.noreply.github.com>
2026-08-24 22:01:15 +0530

    Time: 47 ms (13.49%) | Memory: 27.8 MB (6.44%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int func(int i,int j,string &text1, string &text2,vector<vector<int>>&dp){
-        if(i<0 || j<0) return 0;
+    int func(int i,int j,string &text1, string &text2,vector<vector<int>>&dp,int n,int m){
+        if(i>=n || j>=m) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
 
         if(text1[i]==text2[j]){
-            return dp[i][j]=1+func(i-1,j-1,text1,text2,dp);
+            return dp[i][j]=1+func(i+1,j+1,text1,text2,dp,n,m);
         }
         else{
-            return dp[i][j]=0+max(func(i,j-1,text1,text2,dp),func(i-1,j,text1,text2,dp));
+            return dp[i][j]=0+max(func(i,j+1,text1,text2,dp,n,m),func(i+1,j,text1,text2,dp,n,m));
         }
 
     }
@@ -16,9 +16,9 @@ public:
         int n=text1.size();
         int m=text2.size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n,vector<int>(m+1,-1));
 
-        return func(n-1,m-1,text1,text2,dp);
+        return func(0,0,text1,text2,dp,n,m);
         
     }
 };
