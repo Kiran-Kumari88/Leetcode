0d71868625bc6ce12102969130adfb0c473218eb
Kiran-Kumari88 <146402849+Kiran-Kumari88@users.noreply.github.com>
2026-08-29 13:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.54%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,10 +3,11 @@ public:
     int hammingWeight(int n) {
         int N=n;
         int cnt=0;
-        while(N!=0){
-            N = N & (N-1);
-            cnt++;
+        while(N>0){
+            if(N%2==1) cnt++;
+            N=N/2;
         }
+        if(N==1) cnt+=1;
         return cnt;
         
     }
