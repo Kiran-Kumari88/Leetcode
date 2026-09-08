26f658511ffcd90db07a7f7ec13c90c4ed82ffe2
Kiran-Kumari88 <146402849+Kiran-Kumari88@users.noreply.github.com>
2026-09-08 14:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (47.72%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int min=0;//low = minimum possible number of unmatched '('
-        int max=0; //high = maximum possible number of unmatched '('
+        int maxRange = 0;
+        int minRange = 0;
 
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){ //Because balance definitely increases.
-                min=min+1;
-                max=max+1;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                maxRange++;
+                minRange++;
+            } else if (s[i] == ')') {
+                maxRange--;
+                minRange--;
+            } else {
+                maxRange++;
+                minRange--;
             }
-            else if(s[i]==')'){ //Because balance decreases.
-                min=min-1;
-                max=max-1;
-            }
-            else{ // "*"" -> 3 cases
-            //'('  → balance +1
-            //')'  → balance -1
-            //empty → balance same
-
-                min=min-1;
-                max=max+1;
-            }
-            
-            if(min<0) min=0;
-            if(max<0) return false;
+            if (minRange < 0)
+                minRange = 0;
+            if (maxRange < 0)
+                return false;
         }
-        return (min==0); 
+        return (minRange == 0);
     }
 };
